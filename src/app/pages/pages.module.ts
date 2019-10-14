4f6b679318551d17fa2d7048e5d9124b85bc83cb
LordJarJar <col.arcus@gmail.com>
2019-10-14 16:06:37 -0700

    commit

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,9 +5,10 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { OurworkComponent } from './ourwork/ourwork.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { Newpage2Component } from './newpage2/newpage2.component';
 
 @NgModule({
-  declarations: [ OurworkComponent, LoginComponent, RegisterComponent],
+  declarations: [ OurworkComponent, LoginComponent, RegisterComponent, Newpage2Component],
   imports: [
     CommonModule, 
     HomeModule,
